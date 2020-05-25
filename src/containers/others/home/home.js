efec10dd082bad4e9fb26ea945ab3fd57adb049d
unknown <mehrabyangevorg@gmail.com>
2020-05-25 18:39:27 +0400

    25.05.2020 Create Service Requests

--- src/containers/others/home/home.js
+++ src/containers/others/home/home.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './home.scss';
+import { Link } from 'react-router-dom';
+import back from '../../assets/images/back.png';
+import EventTab from '../../elements/EventTab/EventTab';
+import useEventsInfo from '../../Hooks/useEnevtsInfo';
 
-const Home  = () => {
-  return(
-    <div>
-      Home
+const Home = (props) => {
+  const {data} = useEventsInfo();
+
+  return (
+    <div className='home_page'>
+      <div className='home_background'>
+        <div className='advice_registration'>
+          <div className='info_div'>
+            <h1>Sed et vehicula urna</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <Link to={'/registration/candidate'}>
+              <button>REGISTER NOW</button>
+            </Link>
+          </div>
+          <div className='image_wrap'>
+            <img src={back} alt='background_image'/>
+          </div>
+        </div>
+      </div>
+      <div className='employers_wrap'>
+        <h2>UPCOMING EVENTS</h2>
+        <p>Fusce orci dui, accumsan sed odio at, molestie molestie ipsum. Nullam nec congue tortor. Maecenas tincidunt
+          ullamcorper ultricies. Duis consectetur nisl leo.
+        </p>
+        <div className='custom_work'>
+          {
+            data.map((item, id) => {
+              return <EventTab item={item} key={id}/>
+            })
+          }
+        </div>
+        <Link to={'/registration/employer'}>
+          <button>Register as Employer</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/redux/service.js
+++ src/redux/service.js
@@ -21,7 +21,103 @@ function setAuthorizationToken(store, accessToken, accessRole) {
   }
 }
 
+function login(data) {
+  return axios.post(api.LOGIN, data)
+}
+
+function loginDemoUser() {
+  return axios.post(api.LOGIN_DEMO_USER);
+}
+
+function register(data) {
+  return axios.post(api.REGISTER, data)
+}
+
+function getQuestion(params) {
+  return axios.get(api.QUESTIONS_FIND_ONE, {params})
+}
+
+function saveTest(data) {
+  return axios.post(api.TESTS, data)
+}
+
+function getTests() {
+  return axios.get(api.TESTS)
+}
+
+function getTestsResult(id) {
+  return axios.get(api.TESTS_RESULT, {params: {id}})
+}
+
+function saveAnswer(data) {
+  return axios.post(api.ANSWERS, data)
+}
+
+function getTrainings() {
+  return axios.get(api.TRAININGS)
+}
+
+function getNewRefUsersData() {
+  return axios.get(api.NR_USERS_DATA)
+}
+
+function getTestsAndReattempts() {
+  return axios.get(api.TEST_AND_REATTEMPTS)
+}
+
+function getTestsDataByMonth() {
+  return axios.get(api.TESTS_BY_MONTH)
+}
+
+function getUser() {
+  return axios.get(api.GET_USER)
+}
+
+function createBadge(data) {
+  return axios.post(api.CREATE_BADGES, data)
+}
+
+function updateBadge(data) {
+  return axios.post(api.UPDATE_BADGES, data)
+}
+
+function addFeedback(data) {
+  return axios.post(api.USER_FEEDBACK, data)
+}
+
+function subscribeToNewsLetters(data) {
+  return axios.post(api.SUBSCRIBE_NEWSLETTER, data);
+}
+
+function saveUserLandedTime() {
+  return axios.post(api.SAVE_USER_LANDED);
+}
+
+function saveUserSelectedData() {
+  return axios.post(api.SAVE_USER_SELECTED);
+}
+
 export default {
+  login,
   isLoggedIn,
   setAuthorizationToken
+  register,
+  getTrainings,
+  getQuestion,
+  saveTest,
+  saveAnswer,
+  _getUserFromToken,
+  getTests,
+  getNewRefUsersData,
+  getTestsAndReattempts,
+  getTestsDataByMonth,
+  getTestsResult,
+  getUser,
+  createBadge,
+  updateBadge,
+  addFeedback,
+  loginDemoUser,
+  subscribeToNewsLetters,
+  saveUserLandedTime,
+  saveUserSelectedData
 }
